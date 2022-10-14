b2585ce3e02e69b03d74b8e954b2100fe173f0b7
Rajanidnaik <rajani@robosoftin.com>
2022-10-14 17:52:58 +0530

    added angular material

---
+++ Angular_material/material-demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatButtonModule } from '@angular/material/button';
+
+import { MaterialModule } from './material/material.module';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatButtonModule
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular_material/material-demo/src/app/material/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
+
+
+
+const MaterialComponents=[MatButtonModule,MatButtonToggleModule,MatIconModule,MatBadgeModule,MatProgressSpinnerModule,MatProgressBarModule];
+
+
+@NgModule({
+  // declarations: [],
+  imports: [
+    MaterialComponents
+    // CommonModule
+  ],
+  exports:[MaterialComponents]
+})
+export class MaterialModule {
+  
+ }
