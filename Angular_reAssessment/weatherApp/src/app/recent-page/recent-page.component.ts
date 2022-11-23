429a9c273392026b96d9dbaad56f16033645d6e2
Rajanidnaik <rajani@robosoftin.com>
2022-11-23 18:05:46 +0530

    added files

---
+++ Angular_reAssessment/weatherApp/src/app/recent-page/recent-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-page',
+  templateUrl: './recent-page.component.html',
+  styleUrls: ['./recent-page.component.css']
+})
+export class RecentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
